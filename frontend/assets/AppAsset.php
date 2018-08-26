d49b813f43befdf04cc4336294ddea290c062eca
Habib <hbrawnak@gmail.com>
2018-08-26 22:56:12 +0600

    assets added and new fontend added

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,9 +12,25 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        'css/bootstrap.min.css',
+        'css/font-awesome.min.css',
+        'css/prettyPhoto.css',
+        'css/price-range.css',
+        'css/animate.css',
+        'css/main.css',
+        'css/responsive.css',
         'css/site.css',
     ];
     public $js = [
+        'js/html5shiv.js',
+        'js/respond.min.js',
+        'js/jquery.js',
+        'js/bootstrap.min.js',
+        'js/jquery.scrollUp.min.js',
+        'js/price-range.js',
+        'js/jquery.prettyPhoto.js',
+        'js/main.js'
+
     ];
     public $depends = [
         'yii\web\YiiAsset',
