45ad9f56a60fc543a6db1141daeaa521a8a51d24
Habib <hbrawnak@gmail.com>
2018-08-27 10:01:00 +0600

    database added

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -28,7 +28,7 @@ AppAsset::register($this);
 
 <div class="wrap">
     <?php
-    /*NavBar::begin([
+    NavBar::begin([
         'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
@@ -57,7 +57,7 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => $menuItems,
     ]);
-    NavBar::end();*/
+    NavBar::end();
     ?>
 
     <div class="#">
