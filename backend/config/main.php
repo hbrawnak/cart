b8807993f9d02cb753ba222cbe7ad367a5822bec
Habib <hbrawnak@gmail.com>
2018-08-27 10:37:33 +0600

    admin session for admin login

--- backend/config/main.php
+++ backend/config/main.php
@@ -17,7 +17,7 @@ return [
             'csrfParam' => '_csrf-backend',
         ],
         'user' => [
-            'identityClass' => 'common\models\User',
+            'identityClass' => 'common\models\Admin',
             'enableAutoLogin' => true,
             'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
         ],
