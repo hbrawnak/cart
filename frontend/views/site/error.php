64e70e2bdbfbed6f6580af534af978d91ec79131
Habib <hbrawnak@gmail.com>
2018-08-31 00:35:57 +0600

    Custom 404

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -6,22 +6,18 @@
 /* @var $exception Exception */
 
 use yii\helpers\Html;
+use \yii\helpers\Url;
 
 $this->title = $name;
 ?>
-<div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<div class="container text-center">
+    <div class="logo-404">
+        <a href="<?= Url::to(['/site/'])?>"><img src="<?=Url::to('@web/') ?>images/home/logo.png" alt="" /></a>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
+    <div class="content-404">
+        <img src="<?=Url::to('@web/') ?>images/404/404.png" class="img-responsive" alt="" />
+        <h1><b>OPPS!</b> We Couldn’t Find this Page</h1>
+        <p>Uh... So it looks like you brock something. The page you are looking for has up and Vanished.</p>
+        <h2><a href="<?= Url::to(['/site/'])?>">Bring me back Home</a></h2>
+    </div>
 </div>
